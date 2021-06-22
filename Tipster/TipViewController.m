ded3d42c46c2761223af4ff07d2321010d87c66a
Luke Arney <larney@fb.com>
2021-06-22 17:37:06 -0500

    Inital tipster view

---
+++ Tipster/TipViewController.m
@@ -0,0 +1,49 @@
+//
+//  TipViewController.m
+//  Tipster
+//
+//  Created by Luke Arney on 6/22/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)onTap:(id)sender {
+    NSLog(@"hello");
+    
+    [self.view endEditing:true];
+}
+- (IBAction)updateLabels:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    double tip = bill * 0.2;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%f",tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%f",total];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
